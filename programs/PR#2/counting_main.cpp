98d67fbeb51157ce6df273c4eb558e4b6fd60959
cjikaika <cj.ikaika@gmail.com>
2024-04-11 12:00:23 -0700

    PR#2: Added working makefile

--- programs/PR#2/counting_main.cpp
+++ programs/PR#2/counting_main.cpp
@@ -3,3 +3,17 @@
 // CS23 Spring 2024 Program 2
 // Author: CJ Bridgman-Ford
 // Email: cj.ikaika@gmail.com
+
+#include "counting_header.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+  std::cout << "Number of arguments: " << argc - 1 << std::endl; 
+
+  for (int i = 1; i < argc; ++i) {
+    std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+  }
+
+    return 0;
+}
--- programs/PR#2/counting_source.cpp
+++ programs/PR#2/counting_source.cpp
@@ -3,3 +3,9 @@
 // CS23 Spring 2024 Program 2
 // Author: CJ Bridgman-Ford
 // Email: cj.ikaika@gmail.com
+
+#include "counting_header.h"
+
+void counter() {
+
+}
