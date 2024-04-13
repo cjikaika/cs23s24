b79f004567acf66d803608cef6b07a73135c9988
cjikaika <cj.ikaika@gmail.com>
2024-04-13 13:10:41 -0700

    PR#2: Added citations and testing files

--- programs/PR#2/counting_source.cpp
+++ programs/PR#2/counting_source.cpp
@@ -93,6 +93,7 @@ unsigned long long calculate::rderangement(unsigned long long n) {
 
 std::string calculate::derangement(int n) {
 
+    // VIA https://oeis.org/A000166
     std::string dtable[] = {"1","0","1","2","9","44","265","1854","14833","133496","1334961",
         "14684570","176214841","2290792932","32071101049","481066515734","7697064251745",
         "130850092279664","2355301661033953","44750731559645106","895014631192902121",
